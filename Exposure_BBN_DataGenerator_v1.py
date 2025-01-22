9970e293773627ba239aebd739f7dfac3c137c16
Sina Jahangir <mohammadsina.jahangir@gmail.com>
2025-01-21 20:59:10 -0500

    Update Exposure_BBN_DataGenerator_v1.py

--- Exposure_BBN_DataGenerator_v1.py
+++ Exposure_BBN_DataGenerator_v1.py
@@ -31,121 +31,36 @@ THE SOFTWARE.
 import pandas as pd
 import numpy as np
 import os
-import geopandas as gpd
 #%%
 #change directory to the target folder
 os.chdir(r'D:\NRC')
 #%%
-sovi_=pd.read_csv(r"D:\NRC\DA_Calgary_Table.csv")
-#%%
-#seven flood scenarios
-len_sovi=len(sovi_)
-exposure_=np.zeros((len_sovi,1))
+#pre-define a df
+df_temp=pd.DataFrame()
+ii=0
+#RPs
 for kk in [20,100,500]:
-    for ii in range(0,len_sovi):
-        flag_=0
-        try:
-            expos_temp=pd.read_csv(r'D:\NRC\SW_SampleJBA_RP=%d\Exposure_FID_%d_v1.csv'%(kk,ii),index_col=0)
-            length=np.asarray(expos_temp['Maximum class']).reshape((-1,1))
-            if len(expos_temp)==0:
-                flag_=1
-        except Exception as e:
-            print(e)
-            print(ii)
-            flag_=1
-        if flag_==0:
-            temp_1=length
-            exposure_[ii,0]=len(np.nonzero(length)[0])/len(length)*100
-    gdf=gpd.read_file(r"D:\NRC\Exposure_CalgaryDA\CalgaryDA.shp")
-    # min-max scaling
-    #gdf['Exposure']=(exposure_-np.min(exposure_))/(np.max(exposure_)-np.min(exposure_))
-    gdf['Exposure']=exposure_
-    #df_expos=pd.DataFrame(gdf['Exposure'])
-    df_expos=gdf
     name_='Clagary_%d_JBA_SW'%(kk)
-    df_expos.to_csv('ExposureRoad_%s_v1.csv'%(name_),index=None)
-    try:
-        gdf.to_file(r'Exposure_Shapefile_JBA\ExposureRoad_%s_v1.shp'%(name_))
-    except Exception as e:
-        print(e)
-        print('Creating Directory')
-        os.mkdir('Exposure_Shapefile_JBA')
-        gdf.to_file(r'Exposure_Shapefile_JBA\ExposureRoad_%s_v1.shp'%(name_))
-#%%
-#plot libraries
-import matplotlib.pyplot as plt
-import matplotlib.pylab as pylab
-from matplotlib import rcParams
-from mpl_toolkits.axes_grid1 import make_axes_locatable
-#%%
-#plotting options
-params = {'legend.fontsize': 'large',
-         'axes.labelsize': 'x-large',
-         'axes.titlesize':'x-large',
-         'xtick.labelsize':'x-large',
-         'ytick.labelsize':'x-large'}
-pylab.rcParams.update(params)
-rcParams['font.family'] = 'Calibri'
-#%%
-import matplotlib.pyplot as plt
-from mpl_toolkits.axes_grid1 import make_axes_locatable
-
-fig, ax = plt.subplots(1, 1, figsize=(4.5, 4.5), dpi=400)
-
-# Create an axis divider for adjusting the size of the colorbar
-divider = make_axes_locatable(ax)
-cax = divider.append_axes("right", size="5%", pad=-0.5)  # Adjust size and padding for clarity
-
-# Plot the 'Exposure' column as a colored map
-plot = gdf.plot(column='Exposure', ax=ax, cmap='RdYlGn_r', legend=False)
-
-# Plot shapefile on top
-gdf.plot(ax=ax, facecolor='none', edgecolor='gray', linewidth=1, zorder=2)
-
-# Add and customize the colorbar
-sm = plt.cm.ScalarMappable(cmap='RdYlGn_r', norm=plt.Normalize(vmin=gdf['Exposure'].min(), vmax=gdf['Exposure'].max()))
-cbar = fig.colorbar(sm, cax=cax)
-cbar.set_label('Exposure', fontsize=12, fontweight='bold')
-
-# Set plot title and axis labels
-ax.set_title('Exposure-RP=%d'%(kk), fontsize=16, fontweight='bold')
-ax.set_xlabel('Longitude', fontsize=14)
-ax.set_ylabel('Latitude', fontsize=14)
-
-# Calculate the center of the boundary
-xmin, ymin, xmax, ymax = gdf.total_bounds
-x_center = -114
-y_center = 51
-
-# Define a zoom factor (0.15 means 15% of the total extent)
-zoom_factor = 0.1
-x_range = (xmax - xmin) * zoom_factor
-y_range = (ymax - ymin) * zoom_factor
-
-# Set axis limits to zoom into the center
-ax.set_xlim(x_center - x_range, x_center + x_range)
-ax.set_ylim(y_center - y_range, y_center + y_range)
-
-# Add credit and description text
-fig.text(0.01, 0.06, 'Credit: Jahangir and Shirkhani, NRC (2024)', ha='left', va='bottom', fontsize=12)
-fig.text(0.01, 0.09, 'Flood exposure', ha='left', va='bottom', fontsize=12)
-
-# Adjust layout and save the figure
-plt.tight_layout()
-plt.savefig('Exposure_SW_CalgaryDA_RP=%d_v1.png'%(kk))
-
-    
-
-    
-    
-
-       
-        
-        
-        
-        
-        
-        
-        
-    
-    
+    df=pd.read_csv('ExposureRoad_%s_v1.csv'%(name_))
+    #population density
+    pop_dense=df['pop']/df['a']
+    df_temp['pop_d']=df['pop']/df['a']
+    #return period
+    df_temp['rp']=kk
+    #population density class
+    df_temp['pop_d_c']='Moderate'
+    df_temp['pop_d_c'][pop_dense<np.nanpercentile(pop_dense, 30)]='Low'
+    df_temp['pop_d_c'][pop_dense>np.nanpercentile(pop_dense, 95)]='High'
+    expose=df['Exposure']
+    #exposure
+    df_temp['f_exp']=expose
+    #exposure class
+    df_temp['f_exp_c']='Moderate'
+    df_temp['f_exp_c'][expose<np.nanpercentile(expose, 30)]='Low'
+    df_temp['f_exp_c'][expose>np.nanpercentile(expose, 95)]='High'
+    if ii==0:
+        df_total=df_temp
+    else:
+        df_total=pd.concat((df_total,df_temp),axis=0)
+    ii=ii+1
+df_total.dropna(inplace=True)   
